32b7e74d712c785fa4791a1b7e37655e4f0e8914
Alistair Francis <alistair23@gmail.com>
2014-09-01 22:40:55 +1000

    Netduino_SYSCFG: Update from Peter C's comments on list
    Signed-off-by: Alistair Francis <alistair23@gmail.com>

--- hw/misc/netduino_syscfg.c
+++ hw/misc/netduino_syscfg.c
@@ -1,5 +1,5 @@
 /*
- * Netduino Plus 2 SYSCFG
+ * STM32F405xx Plus 2 SYSCFG
  *
  * Copyright (c) 2014 Alistair Francis <alistair@alistair23.me>
  *
@@ -27,14 +27,17 @@
 #include "hw/hw.h"
 #include "net/net.h"
 
-/* #define DEBUG_NETSYSCFG */
-
-#ifdef DEBUG_NETSYSCFG
-#define DPRINTF(fmt, ...) \
-do { printf("netduino_syscfg: " fmt , ## __VA_ARGS__); } while (0)
-#else
-#define DPRINTF(fmt, ...) do {} while (0)
+#ifndef ST_SYSCFG_ERR_DEBUG
+#define ST_SYSCFG_ERR_DEBUG 0
 #endif
+
+#define DB_PRINT_L(lvl, fmt, args...) do { \
+    if (ST_SYSCFG_ERR_DEBUG >= lvl) { \
+        fprintf(stderr, "stn32f405xx_syscfg: %s:" fmt, __func__, ## args); \
+    } \
+} while (0);
+
+#define DB_PRINT(fmt, args...) DB_PRINT_L(1, fmt, ## args)
 
 #define SYSCFG_MEMRMP  0x00
 #define SYSCFG_PMC     0x04
@@ -44,11 +47,11 @@ do { printf("netduino_syscfg: " fmt , ## __VA_ARGS__); } while (0)
 #define SYSCFG_EXTICR4 0x14
 #define SYSCFG_CMPCR   0x20
 
-#define TYPE_NETDUINO_SYSCFG "netduino_syscfg"
-#define NETDUINO_SYSCFG(obj) \
-    OBJECT_CHECK(struct net_syscfg, (obj), TYPE_NETDUINO_SYSCFG)
+#define TYPE_STM32F405xx_SYSCFG "stn32f405xx-syscfg"
+#define STM32F405xx_SYSCFG(obj) \
+    OBJECT_CHECK(NetSyscfg, (obj), TYPE_STM32F405xx_SYSCFG)
 
-struct net_syscfg {
+typedef struct {
     SysBusDevice parent_obj;
 
     MemoryRegion mmio;
@@ -62,11 +65,11 @@ struct net_syscfg {
     uint32_t syscfg_cmpcr;
 
     qemu_irq irq;
-};
+} NetSyscfg;
 
 static void syscfg_reset(DeviceState *dev)
 {
-    struct net_syscfg *s = NETDUINO_SYSCFG(dev);
+    NetSyscfg *s = STM32F405xx_SYSCFG(dev);
 
     s->syscfg_memrmp = 0x00000000;
     s->syscfg_pmc = 0x00000000;
@@ -77,16 +80,12 @@ static void syscfg_reset(DeviceState *dev)
     s->syscfg_cmpcr = 0x00000000;
 }
 
-static uint64_t netduino_syscfg_read(void *opaque, hwaddr addr,
+static uint64_t stn32f405xx_syscfg_read(void *opaque, hwaddr addr,
                                      unsigned int size)
 {
-    struct net_syscfg *s = opaque;
+    NetSyscfg *s = opaque;
 
-    if (addr >= 0x400) {
-        addr = addr >> 7;
-    }
-
-    DPRINTF("Read 0x%x\n", (uint) addr);
+    DB_PRINT("Read 0x%x\n", (uint) addr);
 
     switch (addr) {
     case SYSCFG_MEMRMP:
@@ -112,28 +111,24 @@ static uint64_t netduino_syscfg_read(void *opaque, hwaddr addr,
     return 0;
 }
 
-static void netduino_syscfg_write(void *opaque, hwaddr addr,
+static void stn32f405xx_syscfg_write(void *opaque, hwaddr addr,
                        uint64_t val64, unsigned int size)
 {
-    struct net_syscfg *s = opaque;
+    NetSyscfg *s = opaque;
     uint32_t value = (uint32_t) val64;
 
-    if (addr >= 0x400) {
-        addr = addr >> 7;
-    }
-
-    DPRINTF("Write 0x%x, 0x%x\n", value, (uint) addr);
+    DB_PRINT("Write 0x%x, 0x%x\n", value, (uint) addr);
 
     switch (addr) {
     case SYSCFG_MEMRMP:
-        /* This isn't supported, so don't allow the guest to write to it
-         * s->syscfg_memrmp = value;
-         */
+        qemu_log_mask(LOG_UNIMP,
+                      "net_syscfg_write: Changeing the memory mapping " \
+                      "isn't supported in QEMU\n");
         return;
     case SYSCFG_PMC:
-        /* This isn't supported, so don't allow the guest to write to it
-         * s->syscfg_pmc = value;
-         */
+        qemu_log_mask(LOG_UNIMP,
+                      "net_syscfg_write: Peripheral mode configuration " \
+                      "isn't supported in QEMU\n");
         return;
     case SYSCFG_EXTICR1:
         s->syscfg_exticr1 = (value & 0xFF);
@@ -156,46 +151,41 @@ static void netduino_syscfg_write(void *opaque, hwaddr addr,
     }
 }
 
-static const MemoryRegionOps netduino_syscfg_ops = {
-    .read = netduino_syscfg_read,
-    .write = netduino_syscfg_write,
+static const MemoryRegionOps stn32f405xx_syscfg_ops = {
+    .read = stn32f405xx_syscfg_read,
+    .write = stn32f405xx_syscfg_write,
     .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
-static int netduino_syscfg_init(SysBusDevice *sbd)
+static void stn32f405xx_syscfg_init(Object *obj)
 {
-    DeviceState *dev = DEVICE(sbd);
-    struct net_syscfg *s = NETDUINO_SYSCFG(dev);
+    NetSyscfg *s = STM32F405xx_SYSCFG(obj);
 
-    sysbus_init_irq(sbd, &s->irq);
+    sysbus_init_irq(SYS_BUS_DEVICE(obj), &s->irq);
 
-    memory_region_init_io(&s->mmio, OBJECT(s), &netduino_syscfg_ops, s,
-                          TYPE_NETDUINO_SYSCFG, 0x2000);
-    sysbus_init_mmio(sbd, &s->mmio);
-
-    return 0;
+    memory_region_init_io(&s->mmio, obj, &stn32f405xx_syscfg_ops, s,
+                          TYPE_STM32F405xx_SYSCFG, 0x2000);
+    sysbus_init_mmio(SYS_BUS_DEVICE(obj), &s->mmio);
 }
 
-static void netduino_syscfg_class_init(ObjectClass *klass, void *data)
+static void stn32f405xx_syscfg_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
-    SysBusDeviceClass *k = SYS_BUS_DEVICE_CLASS(klass);
 
-    k->init = netduino_syscfg_init;
-    dc->props = NULL;
     dc->reset = syscfg_reset;
 }
 
-static const TypeInfo netduino_syscfg_info = {
-    .name          = TYPE_NETDUINO_SYSCFG,
+static const TypeInfo stn32f405xx_syscfg_info = {
+    .name          = TYPE_STM32F405xx_SYSCFG,
     .parent        = TYPE_SYS_BUS_DEVICE,
-    .instance_size = sizeof(struct net_syscfg),
-    .class_init    = netduino_syscfg_class_init,
+    .instance_size = sizeof(NetSyscfg),
+    .instance_init = stn32f405xx_syscfg_init,
+    .class_init    = stn32f405xx_syscfg_class_init,
 };
 
-static void netduino_syscfg_register_types(void)
+static void stn32f405xx_syscfg_register_types(void)
 {
-    type_register_static(&netduino_syscfg_info);
+    type_register_static(&stn32f405xx_syscfg_info);
 }
 
-type_init(netduino_syscfg_register_types)
+type_init(stn32f405xx_syscfg_register_types)
