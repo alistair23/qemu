04950a2323d1285e4f191a3bea769095f9621875
Alistair Francis <alistair23@gmail.com>
2014-09-14 15:24:45 +1000

    target_arm: Parameterise the irq lines for armv7m_init
    This patch allows the board to specifiy the number of NVIC interrupt
    lines when using armv7m_init.
    
    Signed-off-by: Alistair Francis <alistair23@gmail.com>

--- include/hw/arm/arm.h
+++ include/hw/arm/arm.h
@@ -16,7 +16,7 @@
 
 /* armv7m.c */
 qemu_irq *armv7m_init(MemoryRegion *system_memory,
-                      int flash_size, int sram_size,
+                      int flash_size, int sram_size, int num_irq,
                       const char *kernel_filename, const char *cpu_model);
 
 /* arm_boot.c */
